d04885b4ebaf411ff642531ba08623adbd8ca3b2
AkhilDeo <singingbird99@gmail.com>
2022-04-20 15:28:00 -0400

    Decode received messages

--- venv/UDPListener.py
+++ venv/UDPListener.py
@@ -1,11 +1,12 @@
 import socket
 UDP_IP = socket.gethostbyname(socket.gethostname()) # or UDP_IP = socket.gethostbyname(socket.gethostname())
 UDP_PORT = 15002
-
+print("Start")
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(("", UDP_PORT))
 while True:
     data, addr = sock.recvfrom(1024)
-    print("received messages: %s" % data)
+    data = data.decode()
+    print(data)
 
 
