199083d015d00f8fa6aaee91feab887226f7db66
AkhilDeo <singingbird99@gmail.com>
2022-05-17 16:26:49 -0400

    Takes the transmitted JSON Strings and converts them to dictionaries to parse

--- venv/UDPListener.py
+++ venv/UDPListener.py
@@ -1,15 +1,34 @@
 import socket
+import json
 UDP_IP = socket.gethostbyname(socket.gethostname())
 UDP_PORT = 15002
-print("Start")
+#print("Start")
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(("", UDP_PORT))
 while True:
     data, addr = sock.recvfrom(1024)
-    data = data.decode()
-    dataArray = data.split(" ")
+    #data = data.decode()
+    if bool(data):
+        dataDict = json.loads(data)
+        print(dataDict)
+        print(dataDict['slider'])
 
+    #if len(dataArray) == 2:
+     #   gripper = dataArray[1]
+        #Set grip/pincer according to this value
+    #else:
+        # Need to case strings to doubles / bool
+     #   x = dataArray[1]
+      #  y = dataArray[3]
+       # z = dataArray[5]
+        #roll = dataArray[7]
+        #pitch = dataArray[9]
+        #yaw = dataArray[11]
+        #gripper = dataArray[13]
+        #cameraMoving = dataArray[15]
+    # Set AMBF simulation values based on these values
 
-    print(data)
+    # Maybe once cameraMoving = true, set 1st pos as "origin" and
+    # move camera based on that first home/origin val
 
 
