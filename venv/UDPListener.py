f4ff56d7f0215b9fc59fda7f9c774f1514831d49
AkhilDeo <singingbird99@gmail.com>
2022-05-17 16:29:36 -0400

    Removed unnecessary comments

--- venv/UDPListener.py
+++ venv/UDPListener.py
@@ -12,23 +12,3 @@ while True:
         dataDict = json.loads(data)
         print(dataDict)
         print(dataDict['slider'])
-
-    #if len(dataArray) == 2:
-     #   gripper = dataArray[1]
-        #Set grip/pincer according to this value
-    #else:
-        # Need to case strings to doubles / bool
-     #   x = dataArray[1]
-      #  y = dataArray[3]
-       # z = dataArray[5]
-        #roll = dataArray[7]
-        #pitch = dataArray[9]
-        #yaw = dataArray[11]
-        #gripper = dataArray[13]
-        #cameraMoving = dataArray[15]
-    # Set AMBF simulation values based on these values
-
-    # Maybe once cameraMoving = true, set 1st pos as "origin" and
-    # move camera based on that first home/origin val
-
-
