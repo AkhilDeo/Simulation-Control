3e10be670fb8aed521574b5393c552108b2a263e
AkhilDeo <singingbird99@gmail.com>
2022-06-24 23:41:10 -0400

    Implemented servo_cp for PSMRight

--- venv/ros_control_new.py
+++ venv/ros_control_new.py
@@ -12,6 +12,7 @@ import socket
 import json
 import time
 from psm_arm import PSM
+import numpy as np
 
 UDP_IP = socket.gethostbyname(socket.gethostname())
 UDP_PORT = 15002
@@ -40,10 +41,26 @@ _client.connect()
 #print(_client.get_obj_names())
 w = _client.get_world_handle()
 w.reset_bodies()
-psm_left = PSM(_client, 'psm1')
-psm_right = PSM(_client, 'psm2')
-psm_arms = {"left": psm_left,
-            "right": psm_right}
+psm1 = PSM(_client, 'psm1')
+psm2 = PSM(_client, 'psm2')
+psm_arms = {"left": psm1,
+            "right": psm2}
+
+# The PSMs can be controlled either in joint space or cartesian space. For the
+# latter, the `servo_cp` command sets the end-effector pose w.r.t its Base frame.
+
+T_e_b = Frame(Rotation.RPY(np.pi, 0, np.pi/2.), Vector(0., 0., -1.3))
+print("Setting the end-effector frame of PSM1 w.r.t Base", T_e_b)
+psm1.servo_cp(T_e_b)
+psm1.set_jaw_angle(0.2)
+time.sleep(1.0)
+T_e_b = Frame(Rotation.RPY(np.pi, 0, np.pi/4.), Vector(0.1, -0.1, -1.3))
+print("Setting the end-effector frame of PSM2 w.r.t Base", T_e_b)
+psm2.servo_cp(T_e_b)
+psm2.set_jaw_angle(0.5)
+time.sleep(1.0)
+
+# Servo_jp testing
 # psm2.servo_jp([-0.4, -0.22, 1.39, -1.64, -0.37, -0.11])
 # psm2.servo_jp([-0.4, -0.22, 1.39, 0, 0, 0])
 # time.sleep(0.5)
@@ -73,8 +90,8 @@ psm_arms = {"left": psm_left,
 # psm2.servo_jp([-0.4, -0.22, 1.39, 3, -0.37, -0.11])
 # time.sleep(5)
 print("Starting TeleOp")
-rate = rospy.Rate(250)
-cur_slider = 0.5
+rate = rospy.Rate(300)
+cur_slider = 0.4
 while not rospy.is_shutdown():
     data, addr = sock.recvfrom(1024)
     if data is not None:
@@ -82,13 +99,10 @@ while not rospy.is_shutdown():
         robot_arm = psm_arms[dataDict['arm']]
         if 'x' in dataDict:
             if dataDict['arm'] == 'right':
-                # robot_arm.servo_jp(
-                #     [dataDict['x'] * 1.25 - 0.5, (dataDict['y'] * -1.25) - 0.1, (dataDict['z'] * -1.25) + 1.39,
-                #                     (dataDict['roll'] * -1.5), (dataDict['pitch'] * 1.5), dataDict['yaw']])
-                self.cmd_rpy = Rotation.RPY(dataDict['roll'], dataDict['pitch'], dataDict['yaw'])
-                self.cmd_xyz = Vector(dataDict['x'], dataDict['y'], dataDict['z'])
-                self.T_IK = Frame(self.cmd_rpy, self.cmd_xyz)
-                robot_arm.servo_cp(self.T_IK)
+                cmd_rpy = Rotation.RPY(-1 * dataDict['yaw'] + np.pi, dataDict['pitch'], dataDict['roll'])
+                cmd_xyz = Vector(dataDict['x'] + 0.1, dataDict['y'] - 0.1, dataDict['z'] - 1.3)
+                T_IK = Frame(cmd_rpy, cmd_xyz)
+                robot_arm.servo_cp(T_IK)
             else:
                 robot_arm.servo_jp(
                     [dataDict['x'] * 1.25 + 0.6, (dataDict['y'] * -1.25) + 0.1, (dataDict['z'] * -1.25) + 1.39,
